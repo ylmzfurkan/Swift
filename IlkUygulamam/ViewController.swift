b73427c4e03faf12cecbb33f4ca0549df3dd2b25
Furkan Yılmaz <ylmzfurkann@gmail.com>
2023-08-31 00:05:41 +0300

    first commit

---
+++ IlkUygulamam/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  IlkUygulamam
+//
+//  Created by FURKAN YILMAZ on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
